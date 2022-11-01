b5864d192d9837e47ee62d5af3bcf75e2cc62574
Cheung <cheung@tdfcu.com>
2022-11-01 11:44:43 -0400

    implemented fetching question from API and displaying it on canvas

--- ui.py
+++ ui.py
@@ -1,18 +1,19 @@
 from tkinter import *
-import tkinter
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 FONT = ('Arial', 20, 'italic')
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title('Quizzler')
         self.window.config(padx=20, pady=20, background=THEME_COLOR)
         self.canvas = Canvas(width=300, height=250)
         self.question_text = self.canvas.create_text(
-            150, 125, text='Some Question Text', font=FONT)
+            150, 125, text='Some Question Text', font=FONT, width=280)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         self.score_label = Label(text='Score: 0', bg=THEME_COLOR, fg='white')
@@ -27,4 +28,10 @@ class QuizInterface:
         self.right_button.grid(row=2, column=0)
         self.wrong_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
